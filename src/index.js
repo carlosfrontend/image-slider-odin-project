ee1531df1dcec7bbf9e4efb9902e5962531af587
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-03-03 13:21:59 +0100

    Feat: Add DOMContentLoaded event

--- src/index.js
+++ src/index.js
@@ -11,69 +11,72 @@ const nextButton = document.querySelector('.bi-caret-right');
 const previewButton = document.querySelector('.bi-caret-left');
 const circles = [...document.querySelectorAll('.circle')];
 
-// Starts with default picture image1
-let index = 0;
+window.addEventListener('DOMContentLoaded', () => {
+  // Starts with default picture image1
 
-myImageContainer.style.backgroundImage = `url(${myImages[index]})`;
-circles[index].focus();
+  let index = 0;
 
-// Background slide effect function
-
-const setBackgroundAnimation = () => {
   myImageContainer.style.backgroundImage = `url(${myImages[index]})`;
-};
+  circles[index].focus();
 
-// Handler for automatic slider
+  // Background slide effect function
 
-const automaticSilder = () => {
-  if (index < 4) {
-    index += 1;
+  const setBackgroundAnimation = () => {
+    myImageContainer.style.backgroundImage = `url(${myImages[index]})`;
+  };
+
+  // Handler for automatic slider
+
+  const automaticSilder = () => {
+    if (index < 4) {
+      index += 1;
+      setBackgroundAnimation();
+      circles[index].focus();
+    } else {
+      index = 0;
+      setBackgroundAnimation();
+      circles[index].focus();
+    }
+  };
+
+  // Timmer
+
+  let slider = setInterval(automaticSilder, 5000);
+
+  // Click event handlers
+
+  circles.forEach((el) => el.addEventListener('click', (e) => {
+    clearInterval(slider);
+    index = +e.target.getAttribute('tabindex');
     setBackgroundAnimation();
-    circles[index].focus();
-  } else {
-    index = 0;
-    setBackgroundAnimation();
-    circles[index].focus();
-  }
-};
+    slider = setInterval(automaticSilder, 5000);
+  }));
 
-// Timmer
+  nextButton.addEventListener('click', () => {
+    clearInterval(slider);
+    if (index === myImages.length - 1) {
+      index = 0;
+      circles[index].focus();
+      setBackgroundAnimation();
+    } else {
+      index += 1;
+      circles[index].focus();
+      setBackgroundAnimation();
+    }
+    slider = setInterval(automaticSilder, 5000);
+  });
 
-let slider = setInterval(automaticSilder, 5000);
-
-// Click event handlers
-
-circles.forEach((el) => el.addEventListener('click', (e) => {
-  clearInterval(slider);
-  index = +e.target.getAttribute('tabindex');
-  setBackgroundAnimation();
-  slider = setInterval(automaticSilder, 5000);
-}));
-
-nextButton.addEventListener('click', () => {
-  clearInterval(slider);
-  if (index === myImages.length - 1) {
-    index = 0;
-    circles[index].focus();
-    setBackgroundAnimation();
-  } else {
-    index += 1;
-    circles[index].focus();
-    setBackgroundAnimation();
-  }
-  slider = setInterval(automaticSilder, 5000);
-});
-
-previewButton.addEventListener('click', () => {
-  clearInterval(slider);
-  if (index === 0) {
-    index = myImages.length - 1;
-    circles[index].focus();
-    setBackgroundAnimation();
-  } else {
-    index -= 1;
-    circles[index].focus();
-    setBackgroundAnimation();
-  }
-  slider = setInterval(automaticSilder, 5000);
+  previewButton.addEventListener('click', () => {
+    clearInterval(slider);
+    if (index === 0) {
+      index = myImages.length - 1;
+      circles[index].focus();
+      setBackgroundAnimation();
+    } else {
+      index -= 1;
+      circles[index].focus();
+      setBackgroundAnimation();
+    }
+    slider = setInterval(automaticSilder, 5000);
+  });
 });
